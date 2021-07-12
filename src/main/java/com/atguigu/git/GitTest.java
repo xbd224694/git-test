e9cfacb3bd2e5e7b4477c93da5bbacd53d794bc8
xbd224694 <66714027+xbd224694@users.noreply.github.com>
2021-07-12 11:11:36 +0800

    Update GitTest.java
    pull test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello master!");
         System.out.println("hello hot-fix!");
         System.out.println("hello push test!");
+        System.out.println("hello pull test!");
     }
 }
